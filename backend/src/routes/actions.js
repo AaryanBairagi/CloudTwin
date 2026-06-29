c62622b0d7da794023192d10aa8ee07ab766d5a8
AaryanBairagi <aaryanbairagi11@gmail.com>
2026-06-30 00:06:01 +0530

    added routes for approve and reject in action

--- backend/src/routes/actions.js
+++ backend/src/routes/actions.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { query } = require("../db/postgres");
+const { enqueueCommand } = require("../db/redis");
 
 router.get("/" , async(req,res) => {
     try{
@@ -8,7 +9,7 @@ router.get("/" , async(req,res) => {
             `
             SELECT *
             FROM actions
-            ORDER_BY created_at DESC
+            ORDER BY created_at DESC
             `
         );
 
@@ -19,6 +20,46 @@ router.get("/" , async(req,res) => {
     }
 });
 
+router.post("/:id/approve" , async(req,res) => {
+    try{
+        const result = await query(`
+            UPDATE actions
+            SET status = 'approved',
+                approved_at = NOW()
+            WHERE id = $1
+            RETURNING *
+            `,
+        [req.params.id]); //returning * means return the updated row 
 
+        if(result.rows.length === 0) return res.status(404).json({message : "Action Not Found"});
+
+        const action = result.rows[0];
+        await enqueueCommand(action.twin_id , action);
+        return res.status(200).json({message : "Action approved" , action});
+        
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({error : "Error approving the request."});
+    }
+});
+
+router.post("/:id/reject" , async(req,res) => {
+    try{
+        const result = await query(`
+            UPDATE actions
+            SET status = 'rejected' 
+            WHERE id = $1
+            RETURNING *
+            `, [req.params.id]);
+        
+        if(result.rows.length === 0) return res.status(404).json({message : "Action Not Found."});
+        const action = result.rows[0];
+        return res.status(200).json({message : "Action Rejected." , action});
+
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({error : "Error rejecting the request."});
+    }
+});
 
 module.exports = router
